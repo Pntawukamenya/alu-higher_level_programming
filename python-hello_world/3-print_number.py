ff7ebbf9921e611ce598a8c9e44a4e2089a76339
Pntawukamenya <p.ntawukame@alustudent.com>
2023-11-01 03:15:47 -0700

    Initial commit

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 number = 42  # Replace 42 with your desired integer
-print(f"{number}, followed by Battery street,\n")
+number = 333  # Replace 333 with your desired integer
+number = -98  # Replace -98 with your desired integer
+number = 0  # Replace 0 with your desired integer
+number = "42"  # Replace "42" with your desired string
+print(f"{int(number)} Battery street\n")
+
