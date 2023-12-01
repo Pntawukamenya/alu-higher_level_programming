12ffea400fa921b58c29cf854b121f6653cabdf9
Pntawukamenya <p.ntawukame@alustudent.com>
2023-12-01 12:22:02 -0800

    initial commit

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# getting content and displaying just content lenght information 
-curl -sI "$1" | grep "Content-Length" | cut -d' ' -f 2
+# displays the size of the body 
+curl -Is $1 | grep -i "content-length:" | cut -d " " -f 2
